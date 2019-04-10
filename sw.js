76142fa1ef1c8bf8cb6fe15cc8a61a0942914afb
Anthony Halliday <anthony.halliday@outlook.com>
2019-04-10 14:50:50 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,47 @@
+/* global caches, fetch, self */
+
+var VERSION = '15';
+var filesToCache = [
+  //'./sw.js'
+];
+
+this.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(VERSION).then(cache => {
+      return cache.addAll(filesToCache).then(_ => {
+        self.skipWaiting();
+      });
+    })
+  );
+});
+
+this.addEventListener('fetch', function(e) {
+  e.respondWith(caches.match(e.request).then(res => {
+    // If there is no match in the cache, we get undefined back,
+    // in that case go to the network!
+    return res ? res : handleNoCacheMatch(e);
+  }));
+});
+
+this.addEventListener('activate', function(e) {
+  e.waitUntil(caches.keys().then(keys => {
+    return Promise.all(keys.map(k => {
+      if (k !== VERSION) {
+        return caches.delete(k);
+      }
+      return undefined;
+    })).then(_ => {
+      return this.clients.claim();
+    });
+  }));
+});
+
+// fetch from network and put into our cache
+function handleNoCacheMatch(e) {
+  return fetch(e.request).then(res => {
+    return caches.open(VERSION).then(cache => {
+      cache.put(e.request, res.clone());
+      return res;
+    });
+  });
+}
